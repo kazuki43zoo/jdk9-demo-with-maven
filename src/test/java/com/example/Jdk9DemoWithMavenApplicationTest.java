4e2b10c162f23b13b546b3b9a6ad0df33630aa50
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-01-06 23:24:39 +0900

    Update

--- src/test/java/com/example/Jdk9DemoWithMavenApplicationTest.java
+++ src/test/java/com/example/Jdk9DemoWithMavenApplicationTest.java
@@ -8,6 +8,7 @@ public class Jdk9DemoWithMavenApplicationTest {
 
 	@Test
 	public void contextLoads() {
+		System.out.println(System.getProperties());
 		Jdk9DemoWithMavenApplication.Message message = new Jdk9DemoWithMavenApplication.Message("test");
 		Assert.assertThat(message.getText(), Is.is("test"));
 	}
